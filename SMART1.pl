26e23fb7a54e3e7298b803fb77e5b965177ea3c5
Woof Woof <6171578+woofwoof@users.noreply.github.com>
2022-11-20 05:06:29 +0000

    Update SMART1.pl
    Added rules to 'traverse' the semantic net & added a rudimentary menu system interface.

--- SMART1.pl
+++ SMART1.pl
@@ -1,3 +1,103 @@
+% SMART: Social Model Alternative Response Trajectories
+% Dr. Timothy Barker, 2022.
+% Released under MIT License.
+% See https://github.com/woofwoof/SMART/blob/main/README.md
+% Written for SWI-Prolog, see https://www.swi-prolog.org/
+% Save then 'consult' this file.
+% Enter "smart." to execute it at the command line prompt.
+
+% menu system (branches)
+menu_branch("1"):-
+    planets.
+
+menu_branch("2"):-
+    crises.
+
+menu_branch("3"):-
+    planetary_systems.
+
+menu_branch("4"):-
+    human_existential_threats.
+
+menu_branch("5"):-
+    earth_threats.
+	
+menu_branch("6"):-
+    war_types.
+	
+menu_branch("0"):-
+    writeln('BYE!'),
+    sleep(3),
+    halt.
+	
+% catch all
+menu_branch(_):-
+    nl,
+    writeln('Incorrect response!'),
+    writeln('BYE!'),
+    sleep(3),
+    halt.
+
+% menu system (main options & user input)
+smart:-
+    nl,nl,
+    writeln('Social Model Alternative Response Trajectories'),
+    writeln('**********************************************'),
+    nl,
+    writeln('Please enter the corresponding menu number followed by return:'),nl,
+    writeln('1. What planets exist?'),
+    writeln('2. What potential crises exist?'),
+    writeln('3. What are the planetary systems?'),
+    writeln('4. What are current human existential threats?'),
+    writeln('5. What are threats to Earth?'),
+    writeln('6. What are the types of war?'),
+    writeln('0. QUIT'),
+    nl,
+    read_line_to_string(user_input,Q),
+    nl,
+    menu_branch(Q),
+    smart.
+
+% query rules
+
+planets:-
+    forall(is_a(X,planet),
+    (write(X),
+    writeln(' is a planet'))).
+    
+crises:-
+    forall(is_a(Y, crisis),
+    (write(Y),
+    writeln(' is a crisis'))).
+
+planetary_systems:-
+    forall(orbits(A,B),
+    (write(A),
+    write(' orbits '),
+    writeln(B))).
+
+human_existential_threats:-
+    are(humans,Y),
+    are(Y,threatened),
+    forall(is_a(Z,crisis),
+    (write(Z),
+    writeln(' threatens humans'))).
+
+earth_threats:-
+    inhabit(X,earth),
+    are(X,threatened),
+    by(threatened,Z),
+    forall(is_a(A,Z),
+    (write('earth is threatened by '),
+    writeln(A))).
+	
+war_types:-
+    forall(can_be(war,Z),
+    (write('war can be '),
+    writeln(Z))).
+
+% semantic net representation
+
 is_a(earth,planet).
 is_a(mars,planet).
 is_a(moon, satellite).
@@ -14,6 +114,8 @@ orbits(moon,earth).
 orbits(earth,sun).
 orbits(mars,sun).
 
+inhabit(species,earth).
+
 are(humans,species).
 are(animals,species).
 are(species,threatened).
@@ -29,6 +131,7 @@ comprises_of(earth,land).
 by(threatened,crisis).
 
 can_be(war,nuclear).
+can_be(war, chemical).
 can_be(war,biological).
 can_be(war,conventional).
 can_be(war,cyber).
